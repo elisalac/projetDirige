0973517a5240fb184d28764de3494137656cb5bc
elisalac <elisa.lacombe@icloud.com>
2023-03-14 10:28:41 -0400

    update index

--- bd.php
+++ bd.php
@@ -1,44 +1,48 @@
 <?php
-function getPdo(){
-    $host    = '127.0.0.1';
-    $db      = 'InscriptionData';
-    $user    = 'elisa';
-    $pass    = 'maxeli1907';
-    $charset = 'utf8';
-    
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    
-    try {
-        return $pdo = new PDO($dsn, $user, $pass, $options);
-    } catch (\PDOException $e) {
-        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    function getPdo(){
+        $host    = '127.0.0.1';
+        $db      = 'InscriptionData';
+        $user    = 'elisa';
+        $pass    = 'maxeli1907';
+        $charset = 'utf8';
+        
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        
+        try {
+            return $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
     }
-}
-function InsertInscription(){
-    $pdo = getPdo();
+    function InsertInscription(){
+        $pdo = getPdo();
 
-    $nom = $_SESSION['nomEnAttenteConfirmation'];
-    $prenom = $_SESSION['prenomEnAttenteConfirmation'];
-    $pseudo = $_SESSION['pseudoEnAttenteConfirmation'];
-    $mdp = $_SESSION['passwordEnAttenteConfirmation'];
-    $courriel = $_SESSION['courrielEnAttenteConfirmation'];
+        $nom = $_SESSION['nomEnAttenteConfirmation'];
+        $prenom = $_SESSION['prenomEnAttenteConfirmation'];
+        $pseudo = $_SESSION['pseudoEnAttenteConfirmation'];
+        $mdp = $_SESSION['passwordEnAttenteConfirmation'];
+        $courriel = $_SESSION['courrielEnAttenteConfirmation'];
 
-    $hash = password_hash($mdp, PASSWORD_DEFAULT);
+        $hash = password_hash($mdp, PASSWORD_DEFAULT);
 
-    try {
-        $sql = "INSERT INTO information (nom, prenom, pseudo, motDePasse, courriel) VALUES (?, ?, ?, ?, ?)";
-        $stmt= $pdo->prepare($sql);
-        $stmt->execute([$nom, $prenom, $pseudo, $mdp, $courriel]);
-    } catch (Exception $e) {
-        echo "Erreur...";
-        exit;
+        try {
+            $sql = "INSERT INTO information (nom, prenom, pseudo, motDePasse, courriel) VALUES (?, ?, ?, ?, ?)";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$nom, $prenom, $pseudo, $mdp, $courriel]);
+        } catch (Exception $e) {
+            echo "Erreur...";
+            exit;
+        }
     }
-}
+
+    function AfficherTousItem(){
+        
+    }
 
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,24 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        if(isset($_POST['tous'])){
+            
+        }
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Recherche</title>
 </head>
+<?php
+    require "include/header.php";
+?>
 <nav>
-    <button></button>
-    <button></button>
-    <button></button>
-    <button></button>
-    <button></button>
+    <form method="post">
+        <input type="submit" value="Tous" name="tous">
+    </form>
 </nav>
 <body>
-
-
-
+    
 </body>
 </html>
