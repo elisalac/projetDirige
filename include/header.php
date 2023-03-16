2504792e3560d3c9252d4c4cf4ea799055b294fd
elisalac <95500009+elisalac@users.noreply.github.com>
2023-03-16 19:47:46 -0400

    Update header.php

--- include/header.php
+++ include/header.php
@@ -2,28 +2,38 @@
 <html>
     <header style="height:75px; width:100%;">
         <div style="position:absolute; left:15px; top:10px;">
-            <form action="profil.php" method="post">
+            <form method="post">
                 <input type="submit" value=" " name="profilButton" style="height:60px; width:60px; border-radius:50%">
             </form>
+
+            <button>Profil</button>
+            <div class="dropdown">
+                <button>Profile</button>
+                <div class="dropdown-options">
+                    <a href="Modifier Profil">Dashboard</a>
+                    <a href="#">Setting</a>
+                    <a href="#">Logout</a>
+                </div>
+            </div>
         </div>
-        <div style="position:absolute; left:1000px; top:25px;">
+        <div style="position:absolute; left:1100px; top:25px;">
             <form action="index.php" method="post">
                 <input type="submit" value="Recherche" name="rechercheButton" style="width:100px; height:35px; font-size:15px; background-color:darkseagreen; border:0px;">
             </form> 
         </div>
-        <div style="position:absolute; left:1200px; top:37px;">
+        <div style="position:absolute; left:1300px; top:25px;">
             <form action="inventaire.php" method="post">
-                <input type="submit" value="Inventaire" name="inventaireButton">
+                <input type="submit" value="Inventaire" name="inventaireButton" style="width:100px; height:35px; font-size:15px; background-color:darkseagreen; border:0px;">
             </form>
         </div>
-        <div style="position:absolute; left:1300px; top:37px;">
+        <div style="position:absolute; left:1500px; top:25px;">
             <form action="panier.php" method="post">
-                <input type="submit" value="Panier" name="panierButton">
+                <input type="submit" value="Panier" name="panierButton" style="width:100px; height:35px; font-size:15px; background-color:darkseagreen; border:0px;">
             </form>
         </div>
-        <div style="position:absolute; left:1400px; top:37px;">
+        <div style="position:absolute; left:1700px; top:25px;">
             <form action="banque.php" method="post">
-                <input type="submit" value="Banque" name="BanqueButton">
+                <input type="submit" value="Banque" name="BanqueButton" style="width:100px; height:35px; font-size:15px; background-color:darkseagreen; border:0px;">
             </form>
         </div>
     </header>
