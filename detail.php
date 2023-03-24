bde9d31b5b08fd5f7f12914e8cb10e9fa9f4b791
momoProg24 <melagboron@gmail.com>
2023-03-24 10:44:55 -0400

    4 fonction + detail
    4 fonction + detail

--- detail.php
+++ detail.php
@@ -4,8 +4,13 @@ require "include/header.php";
 require "include/bd.php";
 
 // faire une verification de c'est quelle item avant de pouvoir essayer c'est quoi mettre la fonctionm qui verifie ele id
-
 #foreach($item as $range)
+$idItem = $_GET["idItems"];
+$_SESSION['idItems'] = $idItem;
+
+
+
+
 ?>
 
 
@@ -16,11 +21,42 @@ require "include/bd.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Détail</title>
 </head>
 
 <body>
-<p>Allo</p>
+
+
+<?php
+
+
+$typeItem = VerifierIdPourtypeItem($idItem);
+while($range = $typeItem->fetch())
+{
+    echo $range['typeItem'] . "<br>";
+   # echo "<p>ALLO</p>";
+}
+
+if($typeItem == 'A')
+{
+    $detailArme =  AfficherDetailArme($idItem);
+    echo "<p>ALLO</p>";
+}
+if($typeItem == 'R')
+{
+    $detailArmure =  AfficherDetailArme($idItem);
+    AfficherDetailArmures($idItem);
+}
+if($typeItem == 'P')
+{
+    AfficherDetailPotion($idItem);
+}
+if($typeItem == 'S')
+{
+    AfficherDetailSorts($idItem);
+}
+
+?>
 </body>
 
 </html>
--- include/bd.php
+++ include/bd.php
@@ -134,75 +134,34 @@ function AfficherDetailArme($idItem)
 {
     $pdo = getPdo();
     $sql = "SELECT * FROM Armes WHERE idImage = ?";
-    if(VerifierIdPourtypeItem($idItem) == 'A')
-    {
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$idItem]);
-
-        while($row = $stmt->fetch(PDO::FETCH_BOTH))
-        {
-            // Quand on va avoir fait un folder_image il faudrat afficher les images 
-            // le  nom de l'item
-            echo "Efficacité de l'arme: ". $row['efficacité'] . "<br>" ."<br>";
-            echo "Description de l'arme: ". $row['description'] . "<br>" ."<br>";
-            echo "Genre de l'arme: ". $row['genre'] . "<br>" ."<br>";
-        }
-    }
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$idItem]);
+    return $stmt;
 }
 
 function AfficherDetailArmures($idItem)
 {
     $pdo = getPdo();
     $sql = "SELECT * FROM Armures WHERE idImage = ?";
-    if(VerifierIdPourtypeItem($idItem) == 'R')
-    {
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$idItem]);
-
-        while($row = $stmt->fetch(PDO::FETCH_BOTH))
-        {
-            // Quand on va avoir fait un folder_image il faudrat afficher les images 
-            // le  nom de l'item
-            echo "Taille de l'arme: ". $row['taille'] . "<br>" ."<br>";
-            echo "Matiere de l'arme: ". $row['matiere'] . "<br>" ."<br>";
-        }
-    }
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$idItem]);
+    return $stmt;
 }
 function AfficherDetailPotion($idItem)
 {
     $pdo = getPdo();
     $sql = "SELECT * FROM Potions WHERE idImage = ?";
-    if(VerifierIdPourtypeItem($idItem) == 'P')
-    {
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$idItem]);
-
-        while($row = $stmt->fetch(PDO::FETCH_BOTH))
-        {
-            // Quand on va avoir fait un folder_image il faudrat afficher les images 
-            // le  nom de l'item
-            echo "Effet attendu: ". $row['effetAttenu'] . "<br>" ."<br>";
-            echo "Durée de l'effet: ". $row['durée'] . "<br>" ."<br>";
-        }
-    }
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$idItem]);
+    return $stmt;
 }
 function AfficherDetailSorts($idItem)
 {
     $pdo = getPdo();
     $sql = "SELECT * FROM Sorts WHERE idImage = ?";
-    if(VerifierIdPourtypeItem($idItem) == 'S')
-    {
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$idItem]);
-
-        while($row = $stmt->fetch(PDO::FETCH_BOTH))
-        {
-            // Quand on va avoir fait un folder_image il faudrat afficher les images 
-            // le  nom de l'item
-            echo "est Instantané: ". $row['estInstantané'] . "<br>" ."<br>";
-            echo "Point de dégats: ". $row['nombreDégatss'] . "<br>" ."<br>";
-        }
-    }
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$idItem]);
+    return $stmt;
 }
 
 
