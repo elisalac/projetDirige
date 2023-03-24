e438f4ac0d1182a837a658c354aaa2914f041d0e
Andrew Kartsakis <andrew.kartsakis@outlook.com>
2023-03-24 11:07:29 -0400

    Update panier.php

--- panier.php
+++ panier.php
@@ -4,17 +4,16 @@
     require "include/bd.php";
     $idJoueur=$_SESSION['id'];
     //La procedure pour payer le panier est prete elle prend en paramètre le id du joueur Call PayerPanier(id)
-  
-    $panier = GetPanierJoueur($idJoueur);
 ?>
 <!DOCTYPE html>
 <html>
     <?php
+       $panier = GetPanierJoueur($idJoueur);
      echo "<table>";
        foreach($panier as $range)
        {
            echo "<tr>";
-               echo "<td>".$range['idItem']."</td>";
+               echo "<td>".$range['IdItem']."</td>";
            echo "</tr>";
        }
     echo "</table>";
