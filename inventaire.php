e38f024022dd9f0f1119741a9581209c1a29e18e
momoProg24 <melagboron@gmail.com>
2023-03-28 10:07:20 -0400

    Update inventaire.php

--- inventaire.php
+++ inventaire.php
@@ -1,8 +1,17 @@
 <?php
     require "include/auth.php";
     require "include/header.php";
+    require "include/bd.php";
+    $idJoueur=$_SESSION['id'];
 ?>
 <!DOCTYPE html>
 <html>
+    <body>
     bruh
+    <?php
+    
+    
+    
+    ?>
+    </body>
 </html>
