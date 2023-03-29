dc7130f2749e71de855052a59c187d33dabbb0ec
momoProg24 <melagboron@gmail.com>
2023-03-29 19:16:50 -0400

    Update inventaire.php

--- inventaire.php
+++ inventaire.php
@@ -1,11 +1,32 @@
 <?php
     require "include/auth.php";
     require "include/header.php";
+    require "include/bd.php";
+    $idJoueur=$_SESSION['id'];
 ?>
 <!DOCTYPE html>
 <html>
     bruh
     <?php
+    
+    $infoItem = AfficherInfoItem($idItem);
+    while($row = $infoItem->fetch())
+    {
+        echo "<div>";
+        echo "Nom de l'item : ". $row['nom'] . "";
+        echo "<br>";
+        echo "<img src='images/Items/". $row['image'] ."' width='200' height='150'>";
+        echo "<br>";
+        echo "</div>";
+    }
+    $itemInv = AfficherInventaireJoueur($idItem);
+    while($row = $itemInv->fetch())
+    {
+        echo "<div>";
+        echo " Quantité dans l'inventaire : ". $row['qteInventaire'] . "";
+        echo "<br>";
+        echo "</div>";
+    }
     
     ?>
 </html>
