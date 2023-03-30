4047398714577a6382d6a8bf11c979c0a65d1bf4
momoProg24 <melagboron@gmail.com>
2023-03-29 22:42:08 -0400

    Update inventaire.php

--- inventaire.php
+++ inventaire.php
@@ -8,24 +8,16 @@
 <html>
     bruh
     <?php
-    
-    $infoItem = AfficherInfoItem($idItem);
-    while($row = $infoItem->fetch())
-    {
-        echo "<div>";
-        echo "Nom de l'item : ". $row['nom'] . "";
-        echo "<br>";
-        echo "<img src='images/Items/". $row['image'] ."' width='200' height='150'>";
-        echo "<br>";
-        echo "</div>";
-    }
+
     $itemInv = AfficherInventaireJoueur($idJoueur);
     while($row = $itemInv->fetch())
     {
+        echo '<a href="http://167.114.152.54/~darquest2/detail.php?idItems=' . $row['idItems'] . '">';
         echo "<div>";
         echo " Quantité dans l'inventaire : ". $row['qteInventaire'] . "";
         echo "<br>";
         echo "</div>";
+        echo '</a>';
     }
     
     ?>
