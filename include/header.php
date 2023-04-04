3e82ffcaec0dfbccb9aa3347d0af51ae82c35957
WillyNilly44 <94925679+WillyNilly44@users.noreply.github.com>
2023-04-04 09:27:23 -0400

    Update header.php

--- include/header.php
+++ include/header.php
@@ -3,6 +3,9 @@
 ?><!DOCTYPE html>
 <html>
     <head>
+        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
         <style>
             .dropdown {
                 display: inline-block;
