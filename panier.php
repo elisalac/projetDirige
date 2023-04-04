0d5525f2defe68ab62f89f4a7234916d36c831ba
Andrew Kartsakis <andrew.kartsakis@outlook.com>
2023-04-04 09:12:06 -0400

    kjchewiufvhiw4fi4

--- panier.php
+++ panier.php
@@ -1,7 +1,6 @@
 <?php
     require "include/auth.php";
     require "include/header.php";
-    require "include/bd.php";
     $idJoueur=$_SESSION['id'];
     //La procedure pour payer le panier est prete elle prend en paramètre le id du joueur Call PayerPanier(id)
 
