b723996419aa120a591dc589e3fa5418d2476cde
momoProg24 <melagboron@gmail.com>
2023-04-27 09:22:27 -0400

    Update banque.php
    y

--- banque.php
+++ banque.php
@@ -1,6 +1,16 @@
 <?php
     require "include/auth.php";
     require "include/header.php";
+    $idJoueur=$_SESSION['id'];
+    $message = "";
+    if ($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        $message = "";
+        if(isset($_POST["demanderPiece"]))
+        {
+            
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,10 +33,35 @@
                 margin:20px;
                 font-size: 20px;
             }
+            .legendPiece{
+                position:absolute;
+                top: 120px;
+                right: 30px;
+                border:1px solid white;
+                padding:10px;
+            }
+            .legendPiece p{
+                color:white;
+                font-size: 17px;
+            }
+            
+
         </style>
     </head>
     <body>
         <hr>
-        <p>Page en développement</p>
+        <p>Demande de pièces à l'admin</p>
+        <div class="legendPiece">
+            <p>1ère demande = 10 Or</p>
+            <p>2ème demande = 10 Argent</p>
+            <p>3ème demand = 10 Bronze</p>
+        </div>
+        <form action="" method="post">
+        <div>
+            <p>Nombre de demandes restantes :</p><?php ?>
+        </div>
+        <input type="submit" value="Envoyer une demande" name="demanderPiece">
+        </form>
+        
     </body>
 </html>
