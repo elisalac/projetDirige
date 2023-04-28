a74c308d11045e28cc32f552daf8d1672d352e80
elisalac <elisa.lacombe@icloud.com>
2023-04-28 10:46:14 -0400

    Update footerEnigma.php

--- include/footerEnigma.php
+++ include/footerEnigma.php
@@ -5,43 +5,10 @@
             footer{
                 position:absolute;
                 bottom:20px;
-                left:40vw;
+                left:42vw;
+                height:100px;
             }
-            .buttonFooter {
-                width:150px;
-                height:50px;
-                --b: 3px;
-                --s: .45em;
-                --color: white;
-                
-                padding: calc(.5em + var(--s)) calc(.9em + var(--s));
-                color: var(--color);
-                --_p: var(--s);
-                background:
-                    conic-gradient(from 90deg at var(--b) var(--b),#0000 90deg,var(--color) 0)
-                    var(--_p) var(--_p)/calc(100% - var(--b) - 2*var(--_p)) calc(100% - var(--b) - 2*var(--_p));
-                transition: .3s linear, color 0s, background-color 0s;
-                outline: var(--b) solid #0000;
-                outline-offset: .6em;
-                font-size: 16px;
-
-                border: 0;
-
-                user-select: none;
-                -webkit-user-select: none;
-                touch-action: manipulation;
-            }
-
-            .buttonFooter:hover,
-            .buttonFooter:focus-visible{
-                --_p: 0px;
-                outline-color: var(--color);
-                outline-offset: .05em;
-            }
-
-            .buttonFooter:active {
-                color: #fff;
-            }
+            
 
             #footerDiv{
                 width:50%;
@@ -49,6 +16,7 @@
                 grid-template-columns: 1fr 1fr;
                 grid-column-gap: 50px;
                 text-align: center;
+                margin-top: 0;
             }
         </style>
         
